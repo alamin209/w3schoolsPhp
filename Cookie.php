c376686c3ff919278f7c471a5e685887212cc49d
abc <you@example.com>
2018-02-24 20:38:29 +0600

    Set cookie,enable cooki,disable cookie,check all are done

--- Cookie.php
+++ Cookie.php
@@ -1,7 +1,43 @@
 <?php
-$Cookie_uesrname="alamin";
-$Cookie_value="alamin Cookie";
-setcookie($Cookie_uesrname,$Cookie_value, time() + (86400*30),"/");
+$Cookie_uesrname="alamin";//change the value
+$Cookie_value="Jahangir Cookie";
+setcookie($Cookie_uesrname,$Cookie_value, time() + (86400*30),"/");//setting the cooki
+//setcookie($Cookie_uesrname,$Cookie_value, time() - 3600,"/");//deleting the cooki
+?>
+<body>
+<?php
+if(!isset($_COOKIE[$Cookie_uesrname]))
+  {
 
+echo "THis  cookie was not seet ".$Cookie_uesrname."</br>";
+
+  }
+  else
+  {
+
+      echo "This cocki is set".$Cookie_uesrname. " </br>";
+      echo "This cocki value".$_COOKIE[$Cookie_uesrname] ." </br>";
+
+  }
 
 ?>
+<?php
+
+//echo  "Cooki user" ."is delete";
+
+if (count($_COOKIE)>0)
+{
+    echo "Cookie are enable";
+
+}
+else
+{
+
+    echo "Cookie are disable";
+}
+
+?>
+
+
+
+</body>
