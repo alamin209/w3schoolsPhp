d139b9d432d455320bb48c16a02369a56849c412
abc <you@example.com>
2018-03-23 10:23:37 +0600

    redaing xml file or string  using parser

---
+++ xmlWithphp.php
@@ -0,0 +1,36 @@
+<?php
+////libxml_use_internal_errors(true);
+//////i should declraed it as true otherwise it will not working coool
+////$xmldastacarrier=
+////
+////"<?xml version='1.0' encoding='UTF-8'?>
+<!--//<note>-->
+//<to>
+//alamin hossain
+//</to>
+//<fromfffsdsd>
+//kahli
+//</fromfffsdsd>
+//<ubjecdddddddt> Hello world</ubjecdddddddt>
+//
+//</note>
+//";
+//
+//$readexml=simplexml_load_string($xmldastacarrier);
+//if ($readexml==false) {
+//    echo 'fail to open the file';
+//    foreach (libxml_get_errors() as $error) {
+//        echo "<br>", $error->message;
+//    }
+//}
+//else
+//{
+//    print_r($readexml);
+//}
+////here is an example to ;load data from xml file
+
+<?php
+//$xml=simplexml_load_file("node.xml") or die("Error: Cannot create object");
+//print_r($xml);
+//?>
+
